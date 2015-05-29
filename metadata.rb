000d8e003a94d94b5c2fadb4b4323e6f8e94bbdd
Lauri Jesmin <lauri.jesmin@cashongo.co.uk>
2015-05-29 11:18:57 +0300

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'cog_wordpress'
+maintainer       'Cash on Go Ltd.'
+maintainer_email 'lauri.jesmin@cashongo.co.uk'
+license          'Apache 2.0'
+description      'Installs/Configures Wordpress'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends          'cog_wpcli','~> 0.1.0'
